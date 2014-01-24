ad95f77507b9235a8b664f97eb220fe7a7924992
jerseejess <jessica.lin8@gmail.com>
2014-01-24 17:20:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workterest::Application.config.secret_key_base = 'caae7e26e6eefba32d2d1b09cd0e4011f327a0308c9dc481259c837b93bbeea7f4de3b38977b211513d084d0cb73a7bc31a6c269ca4973bf3ce803715691c672'
